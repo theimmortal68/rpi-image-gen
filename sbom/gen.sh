06425e32bf89db7bcf0015e853dd6b3c795aaf01
Matthew Lear <matthew.lear@raspberrypi.com>
2024-12-12 10:43:32 +0000

    Add SBOM generation
    Hook up SBOM generation using syft to the end of the post-build stage so
    it takes place immediately before genimage invocation. The output format
    of the SBOM is configurable. If syft is not installed on the host it
    will be downloaded to a location which will be prefixed to the current
    PATH.
    Update documentation.

---
+++ sbom/gen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+
+rootfs=$1
+outdir=$2
+
+SYFT_VER=v1.18.0
+
+# If host has syft, use it
+if ! hash syft 2>/dev/null; then
+   curl -sSfL https://raw.githubusercontent.com/anchore/syft/${SYFT_VER}/install.sh \
+      | sh -s -- -b ${IGconf_work_dir}/host/bin
+fi
+
+SYFT=$(syft --version 2>/dev/null) || die "syft is unusable"
+
+msg "SBOM: $SYFT scanning $rootfs"
+
+podman unshare syft scan dir:"$rootfs" \
+   --base-path "$rootfs" \
+   --source-name "$IGconf_image_name" \
+   --source-version "${IGconf_image_version}" \
+   --output ${IGconf_sbom_output_format}="${outdir}/${IGconf_image_name}.sbom"
