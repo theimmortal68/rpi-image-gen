d4d5dfe74728b6a842b23c707a329babcc7a5bf1
Matthew Lear <matthew.lear@raspberrypi.com>
2025-02-10 15:31:44 +0000

    image: Add BTRFS support
    Add support to mbr/simple_dual so the root filesystem may be created
    as type BTRFS or Ext4, with the latter being the default.
    
    Add a new configuration variable 'image_rootfs_type' specific to the
    mbr/simple_dual layout. This can be passed via config file or options.
    Update layout documentation with details.
    Use the pre-build hook to verify image_rootfs_type value.
    Add btrfs-progs in the list of dependencies.
    
    The value of image_rootfs_type will select the input genimage config to
    be used, as well as mount options in fstab.
    
    Remove rootfstype=ext4 from the templated cmdline.txt. Not supplying
    this argument will mean the kernel probes the device being mounted to
    automatically establish the filesystem to use, which should be fine for
    most deployments (device/image can install their own if required).
    
    Using genimage to create a BTRFS filesystem requires > v18. At the
    current time there is no tagged version of genimage which has BTRFS
    support (added Sept 2024 via #264).
    
    Resolves #13

--- image/mbr/simple_dual/setup.sh
+++ image/mbr/simple_dual/setup.sh
@@ -6,8 +6,22 @@ DISKLABEL=$1
 
 case $DISKLABEL in
    ROOT)
-      cat << EOF > $IMAGEMOUNTPATH/etc/fstab
+      case $IGconf_image_rootfs_type in
+         ext4)
+            cat << EOF > $IMAGEMOUNTPATH/etc/fstab
 /dev/disk/by-label/ROOT /               ext4 rw,relatime,errors=remount-ro 0 1
+EOF
+            ;;
+         btrfs)
+            cat << EOF > $IMAGEMOUNTPATH/etc/fstab
+/dev/disk/by-label/ROOT /               btrfs defaults 0 1
+EOF
+            ;;
+         *)
+            ;;
+      esac
+
+      cat << EOF >> $IMAGEMOUNTPATH/etc/fstab
 /dev/disk/by-label/BOOT /boot/firmware  vfat rw,noatime,fmask=0022,dmask=0022,codepage=437,iocharset=ascii,shortname=mixed,errors=remount-ro 0 2
 EOF
       image2json -g $OUTPUTPATH/genimage.cfg -f $IMAGEMOUNTPATH/etc/fstab > $OUTPUTPATH/image.json
