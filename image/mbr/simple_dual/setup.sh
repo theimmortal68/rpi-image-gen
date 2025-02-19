7694dd9080e2eb3490e5028691e67c2108c2933d
Matthew Lear <matthew.lear@raspberrypi.com>
2025-02-19 13:57:23 +0000

    mbr/simple_dual: switch to using filesystem UUIDs
    Rather than labels, use filesystem UUIDs for mounting and
    identification in this image layout.
    Using UUIDs has a number of advantages including identification being
    transparent across LVM, LUKS, raw block device, etc.
    
    Add uuid-runtime to dependencies.

--- image/mbr/simple_dual/setup.sh
+++ image/mbr/simple_dual/setup.sh
@@ -2,19 +2,21 @@
 
 set -eu
 
-DISKLABEL=$1
+LABEL="$1"
+BOOTUUID="${2:0:4}-${2:4:4}"
+ROOTUUID="$3"
 
-case $DISKLABEL in
+case $LABEL in
    ROOT)
       case $IGconf_image_rootfs_type in
          ext4)
             cat << EOF > $IMAGEMOUNTPATH/etc/fstab
-/dev/disk/by-label/ROOT /               ext4 rw,relatime,errors=remount-ro 0 1
+UUID=${ROOTUUID} /               ext4 rw,relatime,errors=remount-ro 0 1
 EOF
             ;;
          btrfs)
             cat << EOF > $IMAGEMOUNTPATH/etc/fstab
-/dev/disk/by-label/ROOT /               btrfs defaults 0 1
+UUID=${ROOTUUID} /               btrfs defaults 0 1
 EOF
             ;;
          *)
@@ -22,12 +24,12 @@ EOF
       esac
 
       cat << EOF >> $IMAGEMOUNTPATH/etc/fstab
-/dev/disk/by-label/BOOT /boot/firmware  vfat rw,noatime,fmask=0022,dmask=0022,codepage=437,iocharset=ascii,shortname=mixed,errors=remount-ro 0 2
+UUID=${BOOTUUID^^} /boot/firmware  vfat rw,noatime,fmask=0022,dmask=0022,codepage=437,iocharset=ascii,shortname=mixed,errors=remount-ro 0 2
 EOF
       image2json -g $OUTPUTPATH/genimage.cfg -f $IMAGEMOUNTPATH/etc/fstab > $OUTPUTPATH/image.json
       ;;
    BOOT)
-      sed -i "s|root=\([^ ]*\)|root=\/dev\/disk\/by-label\/ROOT|" $IMAGEMOUNTPATH/cmdline.txt
+      sed -i "s|root=\([^ ]*\)|root=UUID=${ROOTUUID}|" $IMAGEMOUNTPATH/cmdline.txt
       ;;
    *)
       ;;
