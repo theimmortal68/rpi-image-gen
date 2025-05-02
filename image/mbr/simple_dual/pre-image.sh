fb7bb495505031aa37e991c3ba8ca34f43a5ec92
Matthew Lear <matthew.lear@raspberrypi.com>
2025-05-02 19:06:29 +0100

    IDP: JSON and build updates
    Align JSON attributes and structure with the ongoing device side
    provisioning development. Add build-time support for Provisioning
    Map (pmap) in image layouts. Main changes:
    
     * JSON: provisionmap is now an array
     * AB: ensure jq parses the pmap array correctly
     * AB(JSON): describe partitions in image order
     * Remove fstab data from the JSON (not needed)
     * JSON: add new attributes (partition alignment, sector size)
     * JSON: standardise on integers for image sizes
     * Rename zero2 -> zero2w
     * Image: config variable added to support pmap selection
     * Image: top-level common pre-build hook added
     * mbr/simple_dual: pmap drives device filesystem setup
     * AB(config): image name updated to avoid overlap

--- image/mbr/simple_dual/pre-image.sh
+++ image/mbr/simple_dual/pre-image.sh
@@ -5,8 +5,13 @@ set -eu
 rootfs=$1
 genimg_in=$2
 
-# Install provision map - TODO select clear/crypt?
-cp ./device/provisionmap.json ${IGconf_sys_outputdir}/provisionmap.json
+SETUP=setup-clear.sh
+
+# Install provision map
+if igconf isset image_pmap ; then
+   cp ./device/provisionmap-${IGconf_image_pmap}.json ${IGconf_sys_outputdir}/provisionmap.json
+   SETUP=setup-${IGconf_image_pmap}.sh
+fi
 
 echo "BOOT_UUID=\"$(uuidgen | sed 's/-.*//')"\" > ${genimg_in}/fs_uuids
 echo "ROOT_UUID=\"$(uuidgen)"\" >> ${genimg_in}/fs_uuids
@@ -20,7 +25,7 @@ cat genimage.cfg.in.$IGconf_image_rootfs_type | sed \
    -e "s|<FW_SIZE>|$IGconf_image_boot_part_size|g" \
    -e "s|<ROOT_SIZE>|$IGconf_image_root_part_size|g" \
    -e "s|<SECTOR_SIZE>|$IGconf_device_sector_size|g" \
-   -e "s|<SETUP>|'$(readlink -ef setup.sh)'|g" \
+   -e "s|<SETUP>|'$(readlink -ef $SETUP)'|g" \
    -e "s|<MKE2FSCONF>|'$(readlink -ef mke2fs.conf)'|g" \
    -e "s|<BOOT_UUID>|$BOOT_UUID|g" \
    -e "s|<ROOT_UUID>|$ROOT_UUID|g" \
