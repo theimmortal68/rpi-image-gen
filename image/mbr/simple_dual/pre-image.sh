530d0128213bbad70664dcbd9951314110456a45
Matthew Lear <matthew.lear@raspberrypi.com>
2025-06-25 11:53:30 +0100

    Improve UUID integration
    Use our pre-defined UUIDs for as many things as possible. UUIDs
    enable transparent handling and identification of block device and are
    agnostic to the underlying media type, eg partitions, LVM, LUKS etc.
    Using UUIDs across the board where possible is a solid improvement.
    
    Add a new specific object to the PMAP JSON to describe static
    information of a partition. This is not currently used by IDP and only
    serves to describe the 'pinned' block devices used for the AB slots.
    
    Add plumbing to the PMAP helper to retrieve the static info and
    introduce some validation.
    
    Since UUIDs are now embedded into the PMAP, AB systems require them to
    be created in time to be plugged into the slot helper. To satisfy that,
    move their generation from image creation to post-chroot as the slot
    helper is needed in the initramfs.
    
    Migrate the slot helper to use UUIDs rather than partition numbers.
    
    Update the AB image slot udev rules to account for invoking the slot
    helper with device UUIDs. Remove other rules since they are no longer
    required.
    
    Linux/udev treats labels specified via mkfs.vfat as a 'UUID' even though
    they're not 128bit. Simplify things and reduce the hackery needed to
    translate a UUID to a mkfs.vfat label by providing a dedicated label to
    the genimage templates.
    
    For crypt PMAPs, specify a UUID for LUKS containers. Provisioning will
    ensure this is accounted for when the containers are created. Being able
    to identify a LUKS container by UUID will greatly simplify cryptroot
    integration.
    
    Remove the dual-crypt AB PMAP. While more than one LUKS container can be
    supported at provisioning, it's unclear if this is something that will
    actually be used.
    
    Bump minor versions for PMAPs that have been updated.

--- image/mbr/simple_dual/pre-image.sh
+++ image/mbr/simple_dual/pre-image.sh
@@ -5,19 +5,29 @@ set -eu
 rootfs=$1
 genimg_in=$2
 
-SETUP=setup-clear.sh
+SETUP=setup.sh
+
 
 # Install provision map
 if igconf isset image_pmap ; then
    cp ./device/provisionmap-${IGconf_image_pmap}.json ${IGconf_sys_outputdir}/provisionmap.json
-   SETUP=setup-${IGconf_image_pmap}.sh
 fi
 
-echo "BOOT_UUID=\"$(uuidgen | sed 's/-.*//')"\" > ${genimg_in}/fs_uuids
-echo "ROOT_UUID=\"$(uuidgen)"\" >> ${genimg_in}/fs_uuids
-. ${genimg_in}/fs_uuids
+
+# Generate pre-defined UUIDs
+BOOT_LABEL=$(uuidgen | sed 's/-.*//' | tr 'a-f' 'A-F')
+BOOT_UUID=$(echo "$BOOT_LABEL" | sed 's/^\(....\)\(....\)$/\1-\2/')
+ROOT_UUID=$(uuidgen)
+CRYPT_UUID=$(uuidgen)
+
+rm -f ${IGconf_sys_outputdir}/img_uuids
+for v in BOOT_LABEL BOOT_UUID ROOT_UUID CRYPT_UUID; do
+    eval "val=\$$v"
+    echo "$v=$val" >> "${IGconf_sys_outputdir}/img_uuids"
+done
 
 
+# Write genimage template
 cat genimage.cfg.in.$IGconf_image_rootfs_type | sed \
    -e "s|<IMAGE_DIR>|$IGconf_sys_outputdir|g" \
    -e "s|<IMAGE_NAME>|$IGconf_image_name|g" \
@@ -27,6 +37,12 @@ cat genimage.cfg.in.$IGconf_image_rootfs_type | sed \
    -e "s|<SECTOR_SIZE>|$IGconf_device_sector_size|g" \
    -e "s|<SETUP>|'$(readlink -ef $SETUP)'|g" \
    -e "s|<MKE2FSCONF>|'$(readlink -ef mke2fs.conf)'|g" \
+   -e "s|<BOOT_LABEL>|$BOOT_LABEL|g" \
    -e "s|<BOOT_UUID>|$BOOT_UUID|g" \
    -e "s|<ROOT_UUID>|$ROOT_UUID|g" \
    > ${genimg_in}/genimage.cfg
+
+
+# Populate PMAP UUIDs
+sed -i \
+   -e "s|<CRYPT_UUID>|$CRYPT_UUID|g" ${IGconf_sys_outputdir}/provisionmap.json
