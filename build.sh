29412c417d9cc869f1c09962cf69e0a61c684a37
Jauschua Stout <jauschua.stout@gmail.com>
2025-08-20 20:00:13 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -440,17 +440,3 @@ elif [ -x ${IGIMAGE}/post-image.sh ] ; then
 else
    runh ${IGTOP_IMAGE}/post-image.sh $IGconf_sys_deploydir
 fi
-
-# Compress all raw .img artefacts (use zstd if you prefer faster decompression)
-shopt -s nullglob
-for img in ci_out/*/artefacts/*.img; do
-  # xz: smallest files (slower to decompress)
-  xz -T0 -9 -f "$img"
-  # Or, comment xz out and use zstd instead:
-  # zstd -19 --threads=0 -f "$img"
-done
-
-# (optional) checksums for release integrity
-for f in ci_out/*/artefacts/*.img; do
-  sha256sum "$f" > "$f.sha256"
-done
