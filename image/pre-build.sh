b38d24e46647c67bcb0d88f3ff56609409cde7a6
Jauschua Stout <jauschua.stout@gmail.com>
2025-08-22 20:04:09 -0400

    Change igconf to ksconf in pre-build.sh

--- image/pre-build.sh
+++ image/pre-build.sh
@@ -2,7 +2,7 @@
 
 set -u
 
-if igconf isset image_pmap ; then
+if ksconf isset image_pmap ; then
    [[ -d "${KS_IMAGE}/device" ]] || die "No device directory for pmap $KSconf_image_pmap"
    [[ -f "${KS_IMAGE}/device/provisionmap-${KSconf_image_pmap}.json" ]] || die "pmap not found"
 fi
